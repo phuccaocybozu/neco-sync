039e82a2030b0d1dd9078c9ce8949c2e7c357fa4
Cao Lương Phúc <phuc-cao@cybozu.vn>
2025-06-05 15:04:30 +0700

    Refactor: structure and code

--- src/sync.sh
+++ src/sync.sh
@@ -1,41 +1,62 @@
-#! /bin/bash
+#!/bin/bash
 #
 # Script to sync files to a Kubernetes pod using Teleport and kubectl
-. ../config/config.sh
 
-unset HTTPS_PROXY
-unset HTTP_PROXY
-unset https_proxy
-unset http_proxy
-echo "-------------------------------------------------------"
-echo "Login to Teleport proxy at $PROXY"
-tsh login --proxy=$PROXY $HOST
+. src/config/config.sh
 
-echo "-------------------------------------------------------"
-echo "Login to $HOST"
-tsh kube login $HOST
+unset_proxies() {
+    unset HTTPS_PROXY HTTP_PROXY https_proxy http_proxy
+}
 
-echo "-------------------------------------------------------"
-echo "Find the name of the pod you want to access"
-kubectl get pods -n $NAME_SPACE | grep $RELEASE_CHANNEL
+login_teleport() {
+    echo "-------------------------------------------------------"
+    echo "Login to Teleport proxy at $PROXY"
+    tsh login --proxy=$PROXY $HOST
+}
 
-echo "-------------------------------------------------------"
-echo "Choose the pod you want to access:"
-read -p "Pod name: " POD
-while [ -z "$POD" ]; do
-    echo "Pod name cannot be empty. Please enter a valid pod name."
+login_kube() {
+    echo "-------------------------------------------------------"
+    echo "Login to $HOST"
+    tsh kube login $HOST
+}
+
+list_pods() {
+    echo "-------------------------------------------------------"
+    echo "Find the name of the pod you want to access"
+    kubectl get pods -n $NAME_SPACE | grep $RELEASE_CHANNEL
+}
+
+select_pod() {
+    echo "-------------------------------------------------------"
+    echo "Choose the pod you want to access:"
     read -p "Pod name: " POD
-done
+    while [ -z "$POD" ]; do
+        echo "Pod name cannot be empty. Please enter a valid pod name."
+        read -p "Pod name: " POD
+    done
+}
 
-echo "-------------------------------------------------------"
-echo "Checking pod $POD"
-kubectl get pod $POD -n $NAME_SPACE
-if [ $? -ne 0 ]; then
-    echo "Pod $POD not found in namespace $NAME_SPACE."
-    exit 1
-fi
+check_pod_exists() {
+    echo "-------------------------------------------------------"
+    echo "Checking pod $POD"
+    kubectl get pod $POD -n $NAME_SPACE
+    if [ $? -ne 0 ]; then
+        echo "Pod $POD not found in namespace $NAME_SPACE."
+        exit 1
+    fi
+}
 
-echo "-------------------------------------------------------"
-echo "Syncing files to pod $POD in namespace $NAME_SPACE"
-kubectl cp -n $NAME_SPACE -c garoon-ap $LOCAL_REPOSITORY/source/$FOLDER_FILES_TO_SYNC $POD:/usr/local/garoon/$FOLDER_FILES_TO_SYNC
+sync_files() {
+    echo "-------------------------------------------------------"
+    echo "Syncing files to pod $POD in namespace $NAME_SPACE"
+    kubectl cp -n $NAME_SPACE -c garoon-ap $LOCAL_REPOSITORY/source/$FOLDER_FILES_TO_SYNC $POD:/usr/local/garoon/$FOLDER_FILES_TO_SYNC
+}
 
+# Main
+unset_proxies
+login_teleport
+login_kube
+list_pods
+select_pod
+check_pod_exists
+sync_files
